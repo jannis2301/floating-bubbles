7a7c5fdb10fa18da3b191df88d364b8dca3981d1
jannis2301 <jannis2301@users.noreply.github.com>
2024-03-07 11:51:55 +0000

    deploy: e8d13be33c1f4088f7a1a5d9c4aa8faa1789bf77

--- src/ts/index.ts
+++
@@ -1,9 +0,0 @@
-import { FloatingBubbles } from './FloatingBubbles';
-
-const backgroundContainer = document.querySelector(
-  '.FB-Background'
-) as HTMLElement;
-
-window.addEventListener('DOMContentLoaded', () => {
-  new FloatingBubbles(backgroundContainer);
-});
